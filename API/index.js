3df6afabce644afb6316d494359b9edfdcaa41d1
MatheusFranco2008 <162757230+MatheusFranco2008@users.noreply.github.com>
2025-08-25 10:14:25 -0300

    base api

---
+++ API/index.js
@@ -0,0 +1,47 @@
+// requires:
+require("dotenv").config();
+const express = require("express");
+const mssql = require('mssql');
+const cors = require("cors");
+const db = require("./db");
+
+const app = express();
+
+const porta = process.env.PORTA;
+const stringSQL = process.env.CONNECTION_STRING;
+
+// Evitar problemas com o CORS
+app.use((request, response, next) =>
+    { 
+      response.header('Access-Control-Allow-Origin','*');
+      next();
+    });
+    
+app.use(cors());
+app.use(express.json());
+
+// função de conectar com o banco de dados
+async function conectaBD() {
+    try {
+        await mssql.connect(stringSQL)
+    } 
+    catch (erro) {
+        console.log(erro)
+    }
+}
+// executa a função para conectar com o banco
+conectaBD()
+
+/******************************* ROTAS/ENDPOINTS *******************************/
+
+
+/****************** COLOCANDO A API NO AR ******************/
+
+app.listen(porta, (error) => {
+    if (error)
+    {
+       console.log("ERRO no Servidor NODEJS rodando na porta 3000");
+       return;
+    } 
+    console.log("Servidor NODEJS rodando na porta 3000!");
+ });
